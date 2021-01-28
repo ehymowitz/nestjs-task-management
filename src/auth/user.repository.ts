f1c90b876886eeb558276afc62a32719a6dea4df
Elie Hymowitz <ehymowitz@cloudraker.com>
2021-01-28 12:05:47 -0500

    signup

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -3,5 +3,12 @@ import { User } from './user.entity';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-  //
+  async signUp(authCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    await user.save();
+  }
 }
